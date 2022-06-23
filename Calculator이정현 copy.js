1fa4468925d690f382ce153eacf0c042439b4ccb
jung <dlwjdgusehd@naver.com>
2022-06-24 00:23:27 +0900

    20220624

---
+++ Calculator이정현 copy.js
@@ -0,0 +1,97 @@
+let val1 = document.getElementById('setValue'); // = 나오는 값
+let result = document.getElementById('getResult'); // = 눌럿을 때 값
+let bt = document.querySelectorAll('button'); //버튼 눌럿을때 작동하기위한 초기변수
+let swc = false;  // 연산자 중복으로 쓰지못하게 하기위한 변수
+let gap = "";
+let stack = 0;
+let npoint = null;
+let dong = "";
+
+for (j = 0; j < bt.length; j++) {
+    bt[j].addEventListener('mouseenter', caculatecol);
+    bt[j].addEventListener('mouseleave', caculatecolback);
+    bt[j].addEventListener('click', caculatenum);
+}
+function caculatecol(event) {
+    event.target.style.background = 'gray';
+}
+function caculatecolback(event) {
+    event.target.style.background = 'linear-gradient(lightgray 20%,white 40%, lightgray 100%)';
+}
+function caculatenum(event) {
+    let val = event.target.classList;
+    if (val == 'num') {
+        val1.innerText += event.target.innerText;
+        swc = false;
+    } else if (val == 'cal') {
+        if (swc == false) {
+            val1.innerText += event.target.innerText;
+            swc = true;
+        }
+    } else if (val == 'back') {
+        if (val1.innerText.length > 0) {
+            val1.innerText = val1.innerText.slice(0, -1);
+        }
+    } else if (val == 'clear') {
+        val1.innerText = "";
+        result.innerText = "";
+    } else if (val == 'result') {
+        for (k = val1.innerText.length; k > 0;) {
+            for (i = 0; i < val1.innerText.length; i++) {
+                if (!(val1.innerText.charAt(i) > -1 || val1.innerText.charAt(i) < 10)) {
+                    npoint = true;
+                    break;
+                } else {
+                    npoint = false;
+                }
+            }
+            if (npoint == true) {
+                for (i = 0; i < val1.innerText.length; i++)
+                if (val1.innerText.charAt(i) > -1 || val1.innerText.charAt(i) < 10) {
+                    gap += val1.innerText.charAt(i);
+                } else {
+                        dong = val1.innerText.charAt(i);
+                        switch (val1.innerText.charAt(i)) {
+                            case '+':
+                                stack = Number(stack) + Number(gap);
+                                break;
+                            case '-':
+                                stack = Number(stack) - Number(gap);
+                                break;
+                            case '*':
+                                stack = Number(stack) * Number(gap);
+                                break;
+                            case '/':
+                                stack = Number(stack) / Number(gap);
+                                break;
+                        }
+                        val1.innerText = val1.innerText.slice(i + 1, val1.innerText.length);
+                        gap = "";
+                        break;
+                    }
+                result.innerText = stack;
+            } else {
+                switch (dong) {
+                    case '+':
+                        stack = Number(stack) + Number(val1.innerText);
+                        break;
+                    case '-':
+                        stack = Number(stack) - Number(val1.innerText);
+                        break;
+                    case '*':
+                        stack = Number(stack) * Number(val1.innerText);
+                        break;
+                    case '/':
+                        stack = Number(stack) / Number(val1.innerText);
+                        break;
+                }
+              
+                val1.innerText = "";
+                gap ="";
+                stack = 0;
+                break;
+            }
+        }
+    }
+}
+
--- Calculator이정현.js
+++ Calculator이정현.js
@@ -1,10 +1,14 @@
-let bt = document.querySelectorAll('button');
+let val1 = document.getElementById('setValue'); // = 나오는 값
+let result = document.getElementById('getResult'); // = 눌럿을 때 값
+let bt = document.querySelectorAll('button'); //버튼 눌럿을때 작동하기위한 초기변수
+let swc = false;  // 연산자 중복으로 쓰지못하게 하기위한 변수
+
+
+
 for(j=0;j<bt.length;j++){
+    bt[j].addEventListener('mouseenter',caculatecol);
     bt[j].addEventListener('mouseleave',caculatecolback);
-}
-for(i=0;i<=9;i++){
-    document.getElementById('num'+i).addEventListener('click',caculatenum);
-    document.getElementById('num'+i).addEventListener('mouseenter',caculatecol);
+    bt[j].addEventListener('click',caculatenum);
 }
 function caculatecol(event){
     event.target.style.background='gray';
@@ -12,10 +16,25 @@ function caculatecol(event){
 function caculatecolback(event){
     event.target.style.background='linear-gradient(lightgray 20%,white 40%, lightgray 100%)';
 }
-
-
 function caculatenum(event){
-    let val1 = document.getElementById('setValue');
-    val1.innerText += event.target.innerText;
+    let val = event.target.classList;
+    if(val =='num'){
+        val1.innerText += event.target.innerText;
+        swc = false;
+    }else if(val=='cal'){
+        if(swc==false){
+            val1.innerText += event.target.innerText;
+            swc = true;
+        }
+    }else if(val=='back'){
+        if(val1.innerText.length>0){
+        val1.innerText = val1.innerText.slice(0,-1);
+        }
+    }else if(val=='clear'){
+        val1.innerText = "";
+        result.innerText = "";
+    }else if(val=='result'){
+        result.innerText = eval(val1.innerText);
+    }
 }
 
